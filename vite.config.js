a712df2de3be6f92854630ebcba7840dd53d1a79
wali ix <waliwebdotcom@gmail.com>
2025-07-12 01:45:36 +0700

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite';
+import dotenv from 'dotenv';
+import bootstrap from '@popperjs/core';
+
+dotenv.config();
+
+export default defineConfig({
+  publicDir: 'assets/static',
+  server: {
+    cors: true,
+    strictPort: true,
+  },
+  build: {
+    assetsDir: '',
+    emptyOutDir: true,
+    manifest: true,
+    outDir: `public/app/themes/${process.env.MWP_THEME}/assets`,
+    rollupOptions: {
+      input: 'assets/js/index.js',
+    },
+  },
+  plugins: [
+    {
+      name: 'php',
+      handleHotUpdate({ file, server }) {
+        if (file.endsWith('.php')) {
+          server.ws.send({ type: 'full-reload', path: '*' });
+        }
+      },
+    },
+    //bootstrap(),
+  ],
+});
